ca0634a919a353b242203ef8487aa0b3ae6aa122
Lorenzo <104233199+lorenzzofiore@users.noreply.github.com>
2023-01-03 17:43:52 -0300

    Add files via upload

---
+++ js/nav.js
@@ -0,0 +1,44 @@
+function openNav(){
+    document .getElementById("mobile-menu").style.width = "100%";
+}
+
+function closeNav(){
+    document .getElementById("mobile-menu").style.width = "0%";
+}
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
